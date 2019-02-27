d645ab463e7ce4874f00efdc34cd7c248e6ed403
Ross <rdaly525@stanford.edu>
2019-02-27 13:45:13 -0800

    changed asserts to type errors, renamed annotations, used functools.wraps

--- peak/peak.py
+++ peak/peak.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from bit_vector import BitVector
+import functools
 
 class Peak:
     pass
@@ -7,37 +8,42 @@ class Peak:
 
 def name_outputs(**outputs):
     """Decorator meant to apply to any function to specify output types
-    The output types will be stored in fn.__peak_outputs__
-    The input types will be stored in fn.__peak_inputs__
+    The output types will be stored in fn.___peak_outputs___
+    The input types will be stored in fn.___peak_inputs___
     Will verify that all the inputs have type annotations
     Will also verify that the outputs of running fn will have the correct number of bits
     """
+
     def decorator(call_fn):
+        @functools.wraps(call_fn)
         def call_wrapper(*args,**kwargs):
             results = call_fn(*args,**kwargs)
             single_output = not isinstance(results,tuple)
             if single_output:
                 results = (results,)
             for i, (oname,otype) in enumerate(outputs.items()):
-                assert results[i].num_bits == otype(0).num_bits, f"{results[i].num_bits} != {otype(0).num_bits}"
+                if results[i].num_bits != otype(0).num_bits:
+                    raise TypeError(f"{results[i].num_bits} != {otype(0).num_bits}")
             if single_output:
                 results = results[0]
             return results
 
         #Set all the outputs
-        call_wrapper.__peak_outputs__ = OrderedDict()
+        call_wrapper._peak_outputs_ = OrderedDict()
         for oname,otype in outputs.items():
-            assert issubclass(otype,BitVector), f"{oname} is not a Bitvector class"
-            call_wrapper.__peak_outputs__[oname] = otype
+            if not issubclass(otype,BitVector):
+                raise TypeError(f"{oname} is not a Bitvector class")
+            call_wrapper._peak_outputs_[oname] = otype
 
         #set all the inputs
-        call_wrapper.__peak_inputs__ = OrderedDict()
+        call_wrapper._peak_inputs_ = OrderedDict()
         num_inputs = call_fn.__code__.co_argcount
         input_names = call_fn.__code__.co_varnames[1:num_inputs]
         in_types = call_fn.__annotations__
-        assert set(input_names) == set(in_types.keys()), "Missing type annotations on inputs"
+        if set(input_names) != set(in_types.keys()):
+            raise TypeError("Missing type annotations on inputs")
         for name in input_names:
-            call_wrapper.__peak_inputs__[name] = in_types[name]
+            call_wrapper._peak_inputs_[name] = in_types[name]
 
         return call_wrapper
     return decorator
