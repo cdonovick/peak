7036ef131b7fc2a0ba752de71515cd5cd9b2c56a
Caleb Donovick <donovick@cs.stanford.edu>
2019-02-28 15:43:17 -0800

    Update test_peak

--- tests/test_peak.py
+++ tests/test_peak.py
@@ -1,9 +1,11 @@
 import pytest
 from peak import *
+from peak.adt import new
+from hwtypes import BitVector
 
 @pytest.mark.parametrize("n", [4])
 def test_bits(n):
-    BitsN = Bits(n)
+    BitsN = new(BitVector, n) 
     for i in range(1<<n):
         bits = BitsN(i)
         assert bits.as_uint() == i
