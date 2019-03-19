e7f2c02338cf6206afccfc146b5db37e7fadca1f
Lenny Truong <lenny@stanford.edu>
2019-03-19 15:52:43 -0700

    Add support for AbstractBit

--- peak/auto_assembler.py
+++ peak/auto_assembler.py
@@ -32,6 +32,8 @@ def get_width(isa : ISABuilder):
         return  max(map(get_width, isa.fields)) + len(isa.fields).bit_length()
     elif _issubclass(isa, AbstractBitVector):
         return isa.size
+    elif _issubclass(isa, AbstractBit) or isinstance(isa, AbstractBit):
+        return 1
     elif isinstance(isa, AbstractBitVector):
         return isa.size
     elif isinstance(isa, int):
@@ -154,6 +156,12 @@ def generate_assembler(isa : ISABuilder):
         return _bv_field(isa)
     elif isinstance(isa, AbstractBitVector):
         return _bv_const(isa)
+    elif _issubclass(isa, AbstractBit):
+        return _bv(isa)
+    elif isinstance(isa, AbstractBit):
+        return _bv_const(isa)
+    elif isinstance(isa, int):
+        return _bv(BitVector(isa))
     else:
-        raise TypeError()
+        raise TypeError(isa)
 
