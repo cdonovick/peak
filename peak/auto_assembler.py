02e669276d4239ca8430a5099d9820c7dbcc8fa5
Caleb Donovick <donovick@cs.stanford.edu>
2019-03-19 13:53:02 -0700

    Move to asm naming

--- peak/auto_assembler.py
+++ peak/auto_assembler.py
@@ -1,7 +1,8 @@
 from .adt import Enum, ISABuilder, Product, Sum, Tuple
+import typing as tp
 from collections import OrderedDict
 from functools import reduce
-from hwtypes import AbstractBitVector,BitVector
+from hwtypes import AbstractBitVector, BitVector, AbstractBit, Bit
 import operator
 
 def _issubclass(sub , parent : type) -> bool:
@@ -10,10 +11,21 @@ def _issubclass(sub , parent : type) -> bool:
     except TypeError:
         return False
 
-
 def get_width(isa : ISABuilder):
     if _issubclass(isa, Enum):
-        return max(i.value for i in isa).bit_length()
+        width = 0
+        for e in map(lambda x : x.value, isa):
+            if isinstance(e, int):
+                width = max(width, e.bit_length())
+            elif isinstance(e, AbstractBitVector):
+                width = max(width, e.size)
+            elif isinstance(e, bool):
+                width = max(width, 1)
+            elif isinstance(e, AbstractBit):
+                width = max(width, 1)
+            else:
+                raise TypeError()
+        return width
     elif _issubclass(isa, (Tuple, Product)):
         return sum(map(get_width, isa.fields))
     elif _issubclass(isa, Sum):
@@ -27,37 +39,34 @@ def get_width(isa : ISABuilder):
     else:
         raise TypeError(isa)
 
-
-
-
 def _enum(isa : Enum):
     encoding = {}
     decoding = {}
     layout = {}
-    w = get_width(isa)
+    width = get_width(isa)
     for inst in isa:
-        layout[inst] = (0, w, None)
-        opcode = BitVector[w](inst.value)
+        layout[inst] = (0, width, None)
+        opcode = BitVector[width](inst.value)
         encoding[inst] = opcode
         decoding[opcode]  = inst
 
-    def encode(inst):
+    def assembler(inst):
         return encoding[inst]
-    def decode(bv):
+    def disassembler(bv):
         return decoding[bv]
 
-    return encode, decode, w, layout
+    return assembler, disassembler, width, layout
 
 def _product(isa : Product):
-    encoding = dict()
-    decoding = dict()
-    layout = dict()
+    encoding = {}
+    decoding = {}
+    layout = {}
 
     width = get_width(isa)
 
     base = 0
     for name,field in isa._fields_dict.items():
-        e, d, w, l = generate_encoder_decoder(field)
+        e, d, w, l = generate_assembler(field)
         encoding[name] = e
         decoding[name] = d
         layout[name] = (base, base+w, l)
@@ -65,21 +74,21 @@ def _product(isa : Product):
 
     assert base == width
 
-    def encode(inst):
+    def assembler(inst):
         opcode = BitVector[width](0)
         for name,v in inst._as_dict().items():
             v = BitVector[width](encoding[name](v))
             opcode |= v << layout[name][0]
         return opcode
 
-    def decode(opcode):
+    def disassembler(opcode):
         args = []
         for name,d in decoding.items():
             base, top, _ = layout[name]
             args.append(d(opcode[base:top]))
         return isa(*args)
 
-    return encode, decode, width, layout
+    return assembler, disassembler, width, layout
 
 def _sum(isa : Sum):
     encoding = {}
@@ -90,13 +99,13 @@ def _sum(isa : Sum):
 
     for tag, field in enumerate(isa.fields):
         tag = BitVector[tag_width](tag)
-        e, d, w, l = generate_encoder_decoder(field)
+        e, d, w, l = generate_assembler(field)
         assert tag_width + w <= width
         encoding[field] = tag, e,
         decoding[tag] = d, w
         layout[field] = (tag_width, tag_width+w, l)
 
-    def encode(inst):
+    def assembler(inst):
         opcode = BitVector[width](0)
         field = type(inst.value)
         tag, e = encoding[field]
@@ -108,22 +117,33 @@ def _sum(isa : Sum):
         opcode |= pay_load << tag_width
         return opcode
 
-    def decode(opcode):
+    def disassembler(opcode):
         tag = opcode[0:tag_width]
         d, w = decoding[tag]
         return isa(d(opcode[tag_width:tag_width + w]))
 
-    return encode, decode, width, layout
+    return assembler, disassembler, width, layout
 
-def _bv(isa : AbstractBitVector):
-    def encode(inst):
+def _bv_field(isa : tp.Type[AbstractBitVector]):
+    def assembler(inst):
         return inst
-    def decode(opcode):
+    def disassembler(opcode):
         return isa(opcode)
-    return encode, decode, get_width(isa), None
+    return assembler, disassembler, get_width(isa), None
 
+def _bv_const(isa : AbstractBitVector):
+    T = type(isa)
+    def assembler(inst):
+        if inst != isa:
+            return ValueError()
+        return inst
+    def disassembler(opcode):
+        if opcode != isa:
+            return ValueError()
+        return T(opcode)
+    return assembler, disassembler, get_width(isa), None
 
-def generate_encoder_decoder(isa : ISABuilder):
+def generate_assembler(isa : ISABuilder):
     if _issubclass(isa, Enum):
         return _enum(isa)
     elif _issubclass(isa, Product):
@@ -131,11 +151,9 @@ def generate_encoder_decoder(isa : ISABuilder):
     elif _issubclass(isa, Sum):
         return _sum(isa)
     elif _issubclass(isa, AbstractBitVector):
-        return _bv(isa)
+        return _bv_field(isa)
     elif isinstance(isa, AbstractBitVector):
-        return _bv(isa)
-    elif isinstance(isa, int):
-        return _bv(BitVector(isa))
+        return _bv_const(isa)
     else:
         raise TypeError()
 
