c6c0f8029cf34e3c28c9a376015bfab0e341320d
Lenny Truong <lenny@stanford.edu>
2019-03-19 23:02:29 -0700

    Add test

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -1,9 +1,13 @@
-from peak.adt import Product, Sum
-from peak.auto_assembler import generate_assembler
+from peak.adt import Product, Sum, Enum
+from peak.auto_assembler import generate_assembler, ISABuilderAssembler, \
+    assemble_values_in_func
 from peak.demo_pes.pe5.isa import INST as pe5_isa
 from peak.arm.isa import Inst as arm_isa
 from peak.pico.isa import Inst as pico_isa
 from hwtypes import BitVector
+import inspect
+import magma as m
+import ast
 
 import pytest
 
@@ -45,3 +49,110 @@ def test_assembler_disassembler(isa):
                 assert isinstance(opcode, BitVector[w])
                 assert d(opcode) == inst
 
+
+def test_ast_rewrite():
+    def gen_cond(enum):
+        class Cond(enum):
+            Z = 0    # EQ
+            Z_n = 1  # NE
+            C = 2    # UGE
+            C_n = 3  # ULT
+            N = 4    # <  0
+            N_n = 5  # >= 0
+            V = 6    # Overflow
+            V_n = 7  # No overflow
+            EQ = 0
+            NE = 1
+            UGE = 2
+            ULT = 3
+            UGT = 8
+            ULE = 9
+            SGE = 10
+            SLT = 11
+            SGT = 12
+            SLE = 13
+            LUT = 14
+            ALU = 15
+        return Cond
+
+    Bit = m.Bit
+    Cond = gen_cond(m.Enum)
+    def cond(code: Cond, alu: Bit, lut: Bit, Z: Bit, N: Bit, C:
+             Bit, V: Bit) -> Bit:
+        if code == Cond.Z:
+            return Z
+        elif code == Cond.Z_n:
+            return not Z
+        elif code == Cond.C or code == Cond.UGE:
+            return C
+        elif code == Cond.C_n or code == Cond.ULT:
+            return not C
+        elif code == Cond.N:
+            return N
+        elif code == Cond.N_n:
+            return not N
+        elif code == Cond.V:
+            return V
+        elif code == Cond.V_n:
+            return not V
+        elif code == Cond.UGT:
+            return C and not Z
+        elif code == Cond.ULE:
+            return not C or Z
+        elif code == Cond.SGE:
+            return N == V
+        elif code == Cond.SLT:
+            return N != V
+        elif code == Cond.SGT:
+            return not Z and (N == V)
+        elif code == Cond.SLE:
+            return Z or (N != V)
+        elif code == Cond.ALU:
+            return alu
+        elif code == Cond.LUT:
+            return lut
+
+    def cond_expected(code: Cond, alu: Bit, lut: Bit, Z: Bit, N: Bit, C: Bit,
+                      V: Bit) -> Bit:
+        if code == assemble(Cond.Z):
+            return Z
+        elif code == assemble(Cond.Z_n):
+            return not Z
+        elif code == assemble(Cond.C) or code == assemble(Cond.UGE):
+            return C
+        elif code == assemble(Cond.C_n) or code == assemble(Cond.ULT):
+            return not C
+        elif code == assemble(Cond.N):
+            return N
+        elif code == assemble(Cond.N_n):
+            return not N
+        elif code == assemble(Cond.V):
+            return V
+        elif code == assemble(Cond.V_n):
+            return not V
+        elif code == assemble(Cond.UGT):
+            return C and not Z
+        elif code == assemble(Cond.ULE):
+            return not C or Z
+        elif code == assemble(Cond.SGE):
+            return N == V
+        elif code == assemble(Cond.SLT):
+            return N != V
+        elif code == assemble(Cond.SGT):
+            return not Z and (N == V)
+        elif code == assemble(Cond.SLE):
+            return Z or (N != V)
+        elif code == assemble(Cond.ALU):
+            return alu
+        elif code == assemble(Cond.LUT):
+            return lut
+
+    assembler, disassembler, width, layout = \
+        generate_assembler(gen_cond(Enum))
+    func_def = m.ast_utils.get_ast(cond).body[0]
+    func_def = ISABuilderAssembler(locals(), globals()).visit(func_def)
+    assert [ast.dump(s) for s in func_def.body] == \
+        [ast.dump(s) for s in m.ast_utils.get_ast(cond_expected).body[0].body]
+
+    # Call the front end to make sure it works
+    cond = assemble_values_in_func(assembler, cond, locals(), globals())
