2886795d4b7be5d09cdceeef9c1709fb4f6013dc
Ross <rdaly525@stanford.edu>
2019-04-02 23:39:25 -0700

    redid coreir isa

--- peak/coreir/sim.py
+++ peak/coreir/sim.py
@@ -3,57 +3,74 @@ import typing as tp
 from .isa import *
 from hwtypes import TypeFamily
 
-def gen_coreir(family : TypeFamily, width=16):
-    Data = family.BitVector[width]
-    Bit = family.Bit
-    Inst = gen_inst_type(family, width)
 
-    class CoreIR(Peak):
-        
-        @name_outputs(out=Data,out_b=Bit)
-        def __call__(self, inst : Inst, in0 : Data, in1 : Data):
-            primitive = inst.primitive
-            const_value = inst.const_value
+def binary_op(inst, in0 : Data, in1 : Data):
+    if inst == BinaryOp.add:
+        res = in0 + in1
+    elif inst == BinaryOp.mul:
+        res = in0 * in1
+    elif inst == BinaryOp.sub:
+        res = in0 - in1
+    elif inst == BinaryOp.or_:
+        res = in0 | in1
+    elif inst == BinaryOp.and_:
+        res = in0 & in1
+    elif inst == BinaryOp.shl:
+        res = in0 << in1
+    elif inst == BinaryOp.lshr:
+        res = in0 >> in1
+    else:
+        raise NotImplementedError
+    return res
 
-            res = Data(0)
-            res_b = Bit(0)
-            if primitive == Primitive.add:
-                res = in0 + in1
-            elif primitive == Primitive.mul:
-                res = in0 * in1
-            elif primitive == Primitive.sub:
-                res = in0 - in1
-            elif primitive == Primitive.or_:
-                res = in0 | in1
-            elif primitive == Primitive.and_:
-                res = in0 & in1
-            elif primitive == Primitive.shl:
-                res = in0 << in1
-            elif primitive == Primitive.lshr:
-                res = in0 >> in1
-            elif primitive == Primitive.not_:
-                res = ~in0
-            elif primitive == Primitive.neg:
-                res = -in0
-            elif primitive == Primitive.eq:
-                res_b = (in0 == in1)
-            elif primitive == Primitive.neq:
-                res = (in0 !=in1)
-            elif primitive == Primitive.ult:
-                res = in0 < in1
-            elif primitive == Primitive.ule:
-                res = in0 <= in1
-            elif primitive == Primitive.ugt:
-                res = in0 > in1
-            elif primitive == Primitive.uge:
-                res = in0 >= in1
-            elif primitive == Primitive.xor:
-                res = in0 ^ in1
-            elif primitive == Primitive.const:
-                res = const_value
-            else:
-                raise NotImplementedError
+def unary_op(inst, in_ : Data):
+    if inst == UnaryOp.not_:
+        res = ~in0
+    elif inst == UnaryOp.neg:
+        res = -in0
+    else:
+        raise NotImplementedError
+    return res
 
-            return res, res_b
+def comp_op(inst, in0 : Data, in1 : Data):
+
+    if inst == CompOp.eq:
+        res_b = in0 == in1
+    elif inst == CompOp.neq:
+        res_b = in0 != in1 
+    elif inst == CompOp.ult:
+        res_b = in0 < in1 
+    elif inst == CompOp.ule:
+        res_b = in0 <= in1 
+    elif inst == CompOp.ugt:
+        res_b = in0 > in1 
+    elif inst == CompOp.uge:
+        res_b = in0 >= in1 
+    else:
+        raise NotImplementedError
+    return res_b
+
+def mask(size):
+    return (Data(1)<<size)-Data(1)
+
+class CoreIR(Peak):
     
-    return CoreIR
+    def __call__(self, inst : Inst, in0 : Data, in1 : Data):
+        kind, inst = inst.match()
+        
+        res = Data(0)
+        res_b = Bit(0)
+        if kind==BinaryOp:
+            res = binary_op(inst,in0,in1)
+        elif kind==UnaryOp:
+            res = unary_op(inst,in0)
+        elif kind==CompOp:
+            res_b = comp_op(inst,in0,in1)
+        elif kind==Const:
+            res = inst.value
+        elif kind==Slice:
+            res = (in0 >> inst.lo) & mask(inst.hi-inst.lo)
+        elif kind ==Concat:
+            res = in0 | (in1<<inst.width0)
+
+        return res, res_b
