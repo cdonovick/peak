5c880c6fd70d262a7c3dfa38e79cfc12f248bd71
Caleb Donovick <donovick@cs.stanford.edu>
2019-05-06 17:30:31 -0700

    remove SMTBitVector

--- peak/__init__.py
+++ peak/__init__.py
@@ -3,6 +3,5 @@ from .register import gen_register
 from .memory import Memory, RAM, ROM
 
 from .peak import Peak, name_outputs, PeakNotImplementedError
-from .mapper.SMT_bit_vector import SMTBit, SMTBitVector, SMTSIntVector
 
 from .rtl_utils import wrap_with_disassembler
