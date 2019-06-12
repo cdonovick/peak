7899c87ebdf4702d26c63a8e5b95d043fa987d93
Lenny Truong <lenny@stanford.edu>
2019-06-12 16:25:47 -0700

    Add magma/fault test for register

---
+++ tests/test_register.py
@@ -0,0 +1,25 @@
+from peak.register import gen_register
+import magma as m
+import fault
+
+
+def test_register():
+    Reg = gen_register(m.get_family(), m.Bits[2], 1)
+    tester = fault.Tester(Reg, Reg.CLK)
+
+    tester.circuit.ASYNCRESET = 0
+    tester.eval()
+    tester.circuit.ASYNCRESET = 1
+    tester.eval()
+    tester.circuit.ASYNCRESET = 0
+    tester.eval()
+    tester.circuit.value = 0
+    tester.circuit.en = 0
+    tester.step(2)
+    tester.circuit.O.expect(1)
+    tester.circuit.value = 2
+    tester.circuit.en = 1
+    tester.step(2)
+    tester.circuit.O.expect(2)
+    tester.compile_and_run("verilator")
+
