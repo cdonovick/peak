37e5fa8dad7a496fbe3f5329e31f5ce50a57992d
Ross <rdaly525@stanford.edu>
2019-07-03 10:33:10 -0700

    fixed pytest errors.

--- peak/__init__.py
+++ peak/__init__.py
@@ -2,6 +2,6 @@
 from .register import gen_register
 from .memory import Memory, RAM, ROM
 
-from .peak import Peak, name_outputs, PeakNotImplementedError
+from .peak import Peak, get_isa, name_outputs, PeakNotImplementedError
 
 from .rtl_utils import wrap_with_disassembler
