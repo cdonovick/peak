f2301d7d5f0c2b8f00cd5d861b3dbf7df40cb9a3
Ross <rdaly525@stanford.edu>
2019-07-06 08:57:47 -0700

    basic working example

--- peak/mapper/mapper.py
+++ peak/mapper/mapper.py
@@ -4,7 +4,7 @@ import functools as ft
 import logging
 from ..peak import Peak, get_isa
 import coreir
-from .binding import Binder, _default_instr
+from .binding import Binder, _default_instr, _get_from_path
 from hwtypes import AbstractBitVector
 from hwtypes import BitVector, SIntVector
 from hwtypes import is_adt_type
@@ -38,6 +38,14 @@ def _tupleify(vals):
         vals = vals,
     return vals
 
+#Will not need 'ordered_dict once hwtypes #57 is resolved
+def _make_adt_instance(rvals,ordered_dict,isa):
+    rvals = _tupleify(rvals)
+    rdict = {}
+    for i,name in enumerate(ordered_dict):
+        rdict[name] = rvals[i]
+    return isa(**rdict)
+
 class ArchMapper:
     def __init__(self,
         arch_fclosure : tp.Callable,
@@ -53,6 +61,7 @@ class ArchMapper:
         self.arch_inputs = arch_smt.__call__._peak_inputs_
         self.arch_outputs = arch_smt.__call__._peak_outputs_
         self.arch_input_isa = _new_product("ArchInput",self.arch_inputs)
+        self.arch_output_isa = _new_product("ArchOutput",self.arch_outputs)
 
         #def isa_filter_fun(inst):
         #    return all(isa_filters(inst) for isa_filter in isa_filters)
@@ -78,6 +87,7 @@ class ArchMapper:
         ir_inputs = ir_smt.__call__._peak_inputs_
         ir_outputs = ir_smt.__call__._peak_outputs_
         ir_input_isa = _new_product("IRInput",ir_inputs)
+        ir_output_isa = _new_product("IROutput",ir_outputs)
 
         found = 0
         if found >= max_mappings:
@@ -87,8 +97,11 @@ class ArchMapper:
         logging.debug("Starting search")
 
         ir_instr = _default_instr(ir_input_isa,forall=True)
-        ir_rvals = _tupleify(ir_smt()(**ir_instr.value_dict))
+        ir_rvals = ir_smt()(**ir_instr.value_dict)
+        ir_output_instr = _make_adt_instance(ir_rvals,ir_outputs,ir_output_isa)
+
         input_binder = Binder(self.arch_input_isa,ir_input_isa,allow_exists=True)
+        output_binder = Binder(self.arch_output_isa,ir_output_isa,allow_exists=False)
         #Early out if no bindings
         if not input_binder.has_binding:
             return
@@ -98,37 +111,38 @@ class ArchMapper:
             for arch_instr,input_binding in input_binder.enumerate_binding(input_binding,ir_instr):
                 #logging.debug(f"inst {ii+1}/{len(self.bv_isa_list)}")
                 #logging.debug(smt_inst)
-                arch_rvals = _tupleify(self.arch_sim(**arch_instr.value_dict))
-                print(arch_rvals)
-                print(input_binding)
-                assert 0
-                for ridx, rval in enumerate(rvals):
-                    assert isinstance(rval, (SMTBit, SMTBitVector))
-                    assert rval.value.get_type() == ir_smt_expr.value.get_type(), f"{rval.value.get_type()} != {ir_smt_expr.value.get_type()}"
+                arch_rvals = (self.arch_sim(**arch_instr.value_dict))
+                arch_output_instr = _make_adt_instance(arch_rvals,self.arch_outputs,self.arch_output_isa)
+                for output_binding in output_binder.enumerate():
+                    output_binding = list(output_binding)
+                    mapping_found = True
+                    for ir_path,arch_path in output_binding:
+                        if not isinstance(ir_path,tuple):
+                            continue
+                        ir_val = _get_from_path(ir_output_instr,ir_path)
+                        arch_val = _get_from_path(arch_output_instr,arch_path)
 
-                    if self.check_equal(binding_dict, rval, ir_smt_expr, name_binding):
-                        #Create output and input map
-                        output_map = {"out":list(ir_outputs.items())[ridx][0]}
-                        input_map = binding
+                        mapping_found &= self.smt_check_equal(ir_val, arch_val)
+                    if mapping_found:
                         mapping = dict(
-                            instruction=self.bv_isa_list[ii],
-                            output_map=output_map,
-                            input_map=input_map
+                            input_binding=input_binding,
+                            output_binding=output_binding
                         )
                         yield mapping
                         found  += 1
                         if found >= max_mappings:
                             return
 
-    def check_equal(self, smt_vars, expr1, expr2, name_binding):
+    def smt_check_equal(self,expr1, expr2):
         with smt.Solver(self.solver_name, logic=QF_BV) as solver:
             expr = expr1 != expr2
             solver.add_assertion(expr.value)
             if not solver.solve():
                 return True
             else:
-                model = solver.get_model()
-                model = {k : model[v.value] for k,v in smt_vars.items()}
-                logging.log(logging.DEBUG - 1, name_binding)
-                logging.log(logging.DEBUG - 1, model)
                 return False
+            #    model = solver.get_model()
+            #    model = {k : model[v.value] for k,v in smt_vars.items()}
+            #    logging.log(logging.DEBUG - 1, name_binding)
+            #    logging.log(logging.DEBUG - 1, model)
+            #    return False
