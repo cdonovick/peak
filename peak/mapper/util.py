d004006fb54d13d3865efddc8a941a82b987d41e
Caleb Donovick <donovick@cs.stanford.edu>
2019-07-09 09:28:40 -0700

    Add SubTypeDict

---
+++ peak/mapper/util.py
@@ -0,0 +1,31 @@
+from collections.abc import MutableMapping
+from collections import deque
+
+class SubTypeDict(MutableMapping):
+    def __init__(self, d=None):
+        if d is None:
+            d = {}
+
+        self._d = d
+
+    def __getitem__(self, key: type):
+        for T in key.mro():
+            try:
+                return self._d[T]
+            except KeyError:
+                pass
+        raise KeyError()
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, type):
+            raise TypeError('Keys must be types')
+        self._d[key] = value
+
+    def __delitem__(self, key):
+        del self._d[key]
+
+    def __iter__(self):
+        return iter(self._d)
+
+    def __len__(self):
+        return len(self._d)
