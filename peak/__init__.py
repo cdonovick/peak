a948a02e30f1f7d2d5860b5b4031ac61880655d8
Ross <rdaly525@stanford.edu>
2019-07-27 15:53:33 -0700

    Added reserved name checking to PeakMeta and a test

--- peak/__init__.py
+++ peak/__init__.py
@@ -2,6 +2,6 @@
 from .register import gen_register
 from .memory import gen_Memory, gen_RAM, gen_ROM
 
-from .peak import Peak, name_outputs, rebind_type, PeakNotImplementedError
+from .peak import Peak, name_outputs, rebind_type, PeakNotImplementedError, ReservedNameError
 
 from .rtl_utils import wrap_with_disassembler
