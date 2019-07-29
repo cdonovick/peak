dfbdb291e060b8fb5c90a3b2efcbeabc85ca1cf9
Ross <rdaly525@stanford.edu>
2019-07-28 19:48:06 -0700

    removed print. Added option to include i64 ops

--- tests/test_wasm.py
+++ tests/test_wasm.py
@@ -1,10 +1,11 @@
-from examples.wasm import WASM
+from examples.wasm import gen_WASM
 from hwtypes import BitVector, Bit
 
 
 Data = BitVector[32]
 
 def test_i32_rotl():
+    WASM = gen_WASM()
     rotl_class = WASM.instructions['i32_rotl']
     assert rotl_class.get_inputs().field_dict == dict(in0=Data,in1=Data)
     assert rotl_class.get_outputs().field_dict == dict(out=Data)
