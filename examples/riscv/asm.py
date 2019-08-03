59035d4990e89ee5d9bd9ac867a73f442c98ca60
Pat Hanrahan <hanrahan@cs.stanford.edu>
2019-08-03 08:51:43 -0700

    Fixed warnings.

--- examples/riscv/asm.py
+++ examples/riscv/asm.py
@@ -20,7 +20,7 @@ def xor(rd, rs1, rs2):
 
 
 def alui(op, rd, rs1, imm):
-    return Inst(ALU(ALUI(op(RD(rd), RS1(rs1), Immed12(imm)))))
+    return Inst(ALU(ALUI(op(RD(rd), RS1(rs1), Immed12(int(imm))))))
 
 def addi(rd, rs1, imm):
     return alui(AddI, rd, rs1, imm)
@@ -42,31 +42,34 @@ def nop():
 
 
 def lui(rd, imm):
-    return Inst(LUI(RD(rd), Immed20(imm)))
+    return Inst(LUI(RD(rd), Immed20(int(imm))))
 
 
 def lw(rd, rs1, imm):
-    return Inst(Memory(LW(RD(rd), RS1(rs1), Immed12(imm))))
+    return Inst(Memory(LW(RD(rd), RS1(rs1), Immed12(int(imm)))))
 
 def sw(rs1, rs2, imm):
-    return Inst(Memory(SW(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return Inst(Memory(SW(RS1(rs1), RS2(rs2), Immed12(int(imm)))))
 
+
+def b(cond, rs1, rs2, imm):
+    return Inst(Branch(cond(RS1(rs1), RS2(rs2), Immed12(int(imm)))))
 
 def beq(rs1, rs2, imm):
-    return Inst(Branch(BEQ(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BEQ, rs1, rs2, imm)
 
 def bne(rs1, rs2, imm):
-    return Inst(Branch(BNE(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BNE, rs1, rs2, imm)
 
 def blt(rs1, rs2, imm):
-    return Inst(Branch(BLT(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BLT, rs1, rs2, imm)
 
 def bge(rs1, rs2, imm):
-    return Inst(Branch(BGE(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BGE, rs1, rs2, imm)
 
 def bltu(rs1, rs2, imm):
-    return Inst(Branch(BLTU(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BLTU, rs1, rs2, imm)
 
 def bgeu(rs1, rs2, imm):
-    return Inst(Branch(BGEU(RS1(rs1), RS2(rs2), Immed12(imm))))
+    return b(BGEU, rs1, rs2, imm)
 
