d5db4421bd4e4528537c9d621153bb94acad3954
Ross <rdaly525@stanford.edu>
2019-08-05 13:17:26 -0700

    cached registers

--- peak/memory.py
+++ peak/memory.py
@@ -7,9 +7,10 @@ def gen_ROM(T, depth : int):
     class ROM(Peak):
         def __init__(self, init : tp.List[T], default_init : T):
             self.mem = []
+            regT = gen_register(T)
             for i in range(depth):
                 data = init[i] if i < len(init) else default_init
-                self.mem.append( gen_register(T)(data))
+                self.mem.append( regT(data))
 
         def __call__(self, addr : int):
             return self.mem[int(addr)](0, Bit(0))
