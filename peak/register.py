45b58c3b61ca435b7f9bc9fbcc2d7d92bcd4addc
Pat Hanrahan <hanrahan@cs.stanford.edu>
2019-08-20 12:11:27 -0700

    Merged new gen_register from paper.

--- peak/register.py
+++ peak/register.py
@@ -1,15 +1,14 @@
 from .peak import Peak
-from hwtypes import BitVector
-import magma as m
+from hwtypes import BitVector, Bit
+import functools
 
-
-def gen_register(family, T, init=0):
+@functools.lru_cache(maxsize=None)
+def gen_register(T):
     class Register(Peak):
-        def __init__(self):
+        def __init__(self, init : T):
             self.value: T = init
 
-        def __call__(self, value: T, en: family.Bit) -> T:
-            assert value is not None
+        def __call__(self, value: T, en: Bit) -> T:
             retvalue = self.value
             if en:
                 self.value = value
@@ -18,7 +17,4 @@ def gen_register(family, T, init=0):
                 # explicitly set it for now
                 self.value = self.value
             return retvalue
-
-    if family.Bit is m.Bit:
-        Register = m.circuit.sequential(Register)
     return Register
