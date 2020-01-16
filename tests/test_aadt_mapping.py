cd099c9977c0920d9c96384789d17dd1b7920fb5
Ross <rdaly525@stanford.edu>
2020-01-16 12:17:33 -0800

    Updated the mapping code to appropriately deal with modifiers. This required updating the binding code to pass in a flat map explicitly For binding, the 'pushed' adt will be used so that ony leaf nodes are modifiered For creating the varmap, the 'stripped' adt will be used so that no values are created as a modified value.

--- tests/test_aadt_mapping.py
+++ tests/test_aadt_mapping.py
@@ -1,6 +1,9 @@
 from hwtypes import Bit, BitVector
+from hwtypes.adt import Enum, Product
+
 from peak.mapper.utils import pretty_print_binding
 from peak.mapper import ArchMapper
+from peak import Const, family_closure, Peak, name_outputs, update_peak
 from examples.min_pe.sim import PE_fc
 from examples.smallir import gen_SmallIR
 
@@ -29,3 +32,45 @@ def test_automapper():
             ir_inputs = solution.build_ir_input(ir_vals)
             arch_inputs = solution.build_arch_input(ir_vals)
             assert ir_bv()(**ir_inputs) == arch_bv()(**arch_inputs)
+
+
+#This will test the const modifier
+def test_const():
+
+    #Testing out something like coreir const
+    @family_closure
+    def IR_fc(family):
+        Data = family.BitVector[16]
+        class IR(Peak):
+            @name_outputs(out=Data)
+            def __call__(self, const_value : Const(Data)):
+                return const_value
+        return update_peak(IR, family)
+
+    @family_closure
+    def Arch_fc(family):
+        Data = family.BitVector[16]
+        class Op(Enum):
+            add = 1
+            const = 2
+        class Inst(Product):
+            op = Op
+            imm = Data
+
+        class Arch(Peak):
+            @name_outputs(out=Data)
+            def __call__(self, inst : Const(Inst), in0 : Data, in1 : Data):
+                if inst.op == Op.add:
+                    return in0 + in1
+                else: #inst.op == Op.const
+                    return inst.imm
+        return update_peak(Arch, family)
+
+    arch_fc = Arch_fc
+    arch_bv = arch_fc(Bit.get_family())
+    arch_mapper = ArchMapper(arch_fc)
+    ir_fc = IR_fc
+    ir_mapper = arch_mapper.process_ir_instruction(ir_fc)
+    solution = ir_mapper.solve('z3')
+    assert solution.solved
+    assert (('const_value',), ('inst', 'imm')) in solution.ibinding
