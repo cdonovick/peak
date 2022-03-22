cb5d180f666f795d53e91e46861228c7bdf1652d
ross <rdaly525@stanford.edu>
2022-03-21 18:02:39 -0700

    Fixes const_rm to generate magma correctly

--- peak/float.py
+++ peak/float.py
@@ -5,7 +5,7 @@ from types import SimpleNamespace
 
 from peak import family_closure, Peak
 from peak.family import SMTFamily, MagmaFamily
-from hwtypes import SMTFPVector, FPVector, RoundingMode as RM, TypeFamily
+from hwtypes import SMTFPVector, FPVector, RoundingMode as RoundingMode_hwtypes, TypeFamily
 from hwtypes.fp_vector_abc import AbstractFPVector
 from peak.black_box import BlackBox
 from hwtypes import BitVector, Bit
@@ -15,14 +15,31 @@ import magma
 class RoundingMode(Enum):
     RNE = 0
     RTZ = 1
-    RDN = 2
-    RUP = 3
-    RMM = 4
+    RDN = 2 #RTN
+    RUP = 3 #RTP
+    RMM = 4 #RNA
 
 def RoudningMode_utils(family):
     RM = RoundingMode
     RM_c = family.get_constructor(RoundingMode)
     BV3 = lambda e: family.BitVector[3](e.value)
+    def RM_to_RM_hwtypes(rm: RoundingMode) -> RoundingMode_hwtypes:
+        return {
+            RoundingMode.RNE: RoundingMode_hwtypes.RNE,
+            RoundingMode.RTZ: RoundingMode_hwtypes.RTZ,
+            RoundingMode.RDN: RoundingMode_hwtypes.RTN,
+            RoundingMode.RUP: RoundingMode_hwtypes.RTP,
+            RoundingMode.RMM: RoundingMode_hwtypes.RNA,
+        }[rm]
+    def RM_hwtypes_to_RM(rm: RoundingMode_hwtypes) -> RoundingMode:
+        return {
+            RoundingMode_hwtypes.RNE: RoundingMode.RNE,
+            RoundingMode_hwtypes.RTZ: RoundingMode.RTZ,
+            RoundingMode_hwtypes.RTN: RoundingMode.RDN,
+            RoundingMode_hwtypes.RTP: RoundingMode.RUP,
+            RoundingMode_hwtypes.RNA: RoundingMode.RMM,
+        }[rm]
+
     def from_rm(rm: RoundingMode) -> BitVector[3]:
         return (rm==RM.RNE).ite(
             BV3(RM.RNE),
@@ -67,15 +84,23 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
     Data = BitVector[width]
 
     def _cast_magma(rm, x):
+        if rm is not RoundingMode_hwtypes.RNE:
+            raise NotImplementedError("Only supports RNE")
         Float = magma.BFloat[width]
         Float.reinterpret_from_bv = lambda bv: Float(bv)
         Float.reinterpret_as_bv = lambda f: magma.Bits[width](f)
+        Float.fp_add = lambda x, y: x + y
+        Float.fp_mul = lambda x, y: x * y
         return Float.reinterpret_from_bv(x)
 
     def _cast_smt(rm, x):
+        if not isinstance(rm, RoundingMode_hwtypes):
+            raise ValueError("Rounding Mode need to be hwtypes version")
         return SMTFPVector[exp_bits, frac_bits, rm, ieee_compliance].reinterpret_from_bv(x)
 
     def _cast_py(rm, x):
+        if not isinstance(rm, RoundingMode_hwtypes):
+            raise ValueError("Rounding Mode need to be hwtypes version")
         return FPVector[exp_bits, frac_bits, rm, ieee_compliance].reinterpret_from_bv(x)
 
     def _get_cast(family):
@@ -86,6 +111,12 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
         else:
             return _cast_py
 
+    def _get_cast_const_rm(rm: RoundingMode, family):
+        rm_hwtypes = RoudningMode_utils(family).RM_to_RM_hwtypes(rm)
+        cast = _get_cast(family)
+        return lambda x: cast(rm_hwtypes, x)
+
+
     @lru_cache(None)
     def gen_binary(op_name):
         @family_closure
@@ -95,29 +126,29 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
             class Op(Peak, BlackBox):
                 def __call__(self, rm: RoundingMode, in0: Data, in1: Data) -> Data:
                     if rm == RoundingMode.RNE:
-                        in0_rne = cast(RM.RNE, in0)
-                        in1_rne = cast(RM.RNE, in1)
+                        in0_rne = cast(RoundingMode_hwtypes.RNE, in0)
+                        in1_rne = cast(RoundingMode_hwtypes.RNE, in1)
                         out_rne = getattr(in0_rne, op_name)(in1_rne)
                         out = out_rne.reinterpret_as_bv()
                     elif rm == RoundingMode.RTZ:
-                        in0_rtz = cast(RM.RTZ, in0)
-                        in1_rtz = cast(RM.RTZ, in1)
+                        in0_rtz = cast(RoundingMode_hwtypes.RTZ, in0)
+                        in1_rtz = cast(RoundingMode_hwtypes.RTZ, in1)
                         out_rtz = getattr(in0_rtz, op_name)(in1_rtz)
                         out = out_rtz.reinterpret_as_bv()
                     elif rm == RoundingMode.RDN:
-                        in0_rdn = cast(RM.RDN, in0)
-                        in1_rdn = cast(RM.RDN, in1)
+                        in0_rdn = cast(RoundingMode_hwtypes.RDN, in0)
+                        in1_rdn = cast(RoundingMode_hwtypes.RDN, in1)
                         out_rdn = getattr(in0_rdn, op_name)(in1_rdn)
                         out = out_rdn.reinterpret_as_bv()
                     elif rm == RoundingMode.RUP:
-                        in0_rup = cast(RM.RUP, in0)
-                        in1_rup = cast(RM.RUP, in1)
+                        in0_rup = cast(RoundingMode_hwtypes.RUP, in0)
+                        in1_rup = cast(RoundingMode_hwtypes.RUP, in1)
                         out_rup = getattr(in0_rup, op_name)(in1_rup)
                         out = out_rup.reinterpret_as_bv()
                     else:
                         assert  rm == RoundingMode.RMM
-                        in0_rmm = cast(RM.RUP, in0)
-                        in1_rmm = cast(RM.RUP, in1)
+                        in0_rmm = cast(RoundingMode_hwtypes.RUP, in0)
+                        in1_rmm = cast(RoundingMode_hwtypes.RUP, in1)
                         out_rmm = getattr(in0_rmm, op_name)(in1_rmm)
                         out = out_rmm.reinterpret_as_bv()
                     return out
@@ -133,29 +164,29 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
             class Op(Peak, BlackBox):
                 def __call__(self, rm: RoundingMode, in0: Data, in1: Data) -> Bit:
                     if rm == RoundingMode.RNE:
-                        in0_rne = cast(RM.RNE, in0)
-                        in1_rne = cast(RM.RNE, in1)
+                        in0_rne = cast(RoundingMode_hwtypes.RNE, in0)
+                        in1_rne = cast(RoundingMode_hwtypes.RNE, in1)
                         out_rne = getattr(in0_rne, op_name)(in1_rne)
                         out = out_rne
                     elif rm == RoundingMode.RTZ:
-                        in0_rtz = cast(RM.RTZ, in0)
-                        in1_rtz = cast(RM.RTZ, in1)
+                        in0_rtz = cast(RoundingMode_hwtypes.RTZ, in0)
+                        in1_rtz = cast(RoundingMode_hwtypes.RTZ, in1)
                         out_rtz = getattr(in0_rtz, op_name)(in1_rtz)
                         out = out_rtz
                     elif rm == RoundingMode.RDN:
-                        in0_rdn = cast(RM.RDN, in0)
-                        in1_rdn = cast(RM.RDN, in1)
+                        in0_rdn = cast(RoundingMode_hwtypes.RDN, in0)
+                        in1_rdn = cast(RoundingMode_hwtypes.RDN, in1)
                         out_rdn = getattr(in0_rdn, op_name)(in1_rdn)
                         out = out_rdn
                     elif rm == RoundingMode.RUP:
-                        in0_rup = cast(RM.RUP, in0)
-                        in1_rup = cast(RM.RUP, in1)
+                        in0_rup = cast(RoundingMode_hwtypes.RUP, in0)
+                        in1_rup = cast(RoundingMode_hwtypes.RUP, in1)
                         out_rup = getattr(in0_rup, op_name)(in1_rup)
                         out = out_rup
                     else:
                         assert  rm == RoundingMode.RMM
-                        in0_rmm = cast(RM.RUP, in0)
-                        in1_rmm = cast(RM.RUP, in1)
+                        in0_rmm = cast(RoundingMode_hwtypes.RUP, in0)
+                        in1_rmm = cast(RoundingMode_hwtypes.RUP, in1)
                         out_rmm = getattr(in0_rmm, op_name)(in1_rmm)
                         out = out_rmm
                     return out
@@ -171,79 +202,29 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
             class Op(Peak, BlackBox):
                 def __call__(self, rm: RoundingMode, in0: Data) -> Data:
                     if rm == RoundingMode.RNE:
-                        in0_rne = cast(RM.RNE, in0)
+                        in0_rne = cast(RoundingMode_hwtypes.RNE, in0)
                         out_rne = getattr(in0_rne, op_name)()
                         out = out_rne.reinterpret_as_bv()
                     elif rm == RoundingMode.RTZ:
-                        in0_rtz = cast(RM.RTZ, in0)
+                        in0_rtz = cast(RoundingMode_hwtypes.RTZ, in0)
                         out_rtz = getattr(in0_rtz, op_name)()
                         out = out_rtz.reinterpret_as_bv()
                     elif rm == RoundingMode.RDN:
-                        in0_rdn = cast(RM.RDN, in0)
+                        in0_rdn = cast(RoundingMode_hwtypes.RDN, in0)
                         out_rdn = getattr(in0_rdn, op_name)()
                         out = out_rdn.reinterpret_as_bv()
                     elif rm == RoundingMode.RUP:
-                        in0_rup = cast(RM.RUP, in0)
+                        in0_rup = cast(RoundingMode_hwtypes.RUP, in0)
                         out_rup = getattr(in0_rup, op_name)()
                         out = out_rup.reinterpret_as_bv()
                     else:
                         assert  rm == RoundingMode.RMM
-                        in0_rmm = cast(RM.RUP, in0)
+                        in0_rmm = cast(RoundingMode_hwtypes.RUP, in0)
                         out_rmm = getattr(in0_rmm, op_name)()
                         out = out_rmm.reinterpret_as_bv()
                     return out
             return Op
         return fc
-
-    def gen_const_rm_binary(op_name, rm, closures):
-        @family_closure
-        def fc(family):
-            rm_ = family.get_constructor(RoundingMode)(rm)
-            _Op = closures[_format_name(op_name)](family)
-            @family.assemble(locals(), globals())
-            class Op(Peak):
-                def __init__(self):
-                    self.op : _Op = _Op()
-
-                def __call__(self, in0: Data, in1: Data) -> Data:
-                    return self.op(rm_, in0, in1)
-
-            return Op
-        return fc
-
-    def gen_const_rm_binary_bit(op_name, rm, closures):
-        @family_closure
-        def fc(family):
-            rm_ = family.get_constructor(RoundingMode)(rm)
-            _Op = closures[_format_name(op_name)](family)
-            @family.assemble(locals(), globals())
-            class Op(Peak):
-                def __init__(self):
-                    self.op : _Op = _Op()
-
-                def __call__(self, in0: Data, in1: Data) -> Bit:
-                    return self.op(rm_, in0, in1)
-
-            return Op
-        return fc
-
-    def gen_const_rm_unary(op_name, rm, closures):
-        @family_closure
-        def fc(family):
-            rm_ = family.get_constructor(RoundingMode)(rm)
-            _Op = closures[_format_name(op_name)](family)
-            @family.assemble(locals(), globals())
-            class Op(Peak):
-                def __init__(self):
-                    self.op : _Op = _Op()
-
-                def __call__(self, in0: Data) -> Data:
-                    return self.op(rm_, in0)
-
-            return Op
-        return fc
-
-
 
     closures = {}
     for k, f in AbstractFPVector.__dict__.items():
@@ -262,34 +243,34 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
         class FMA(Peak, BlackBox):
             def __call__(self, rm: RoundingMode, in0: Data, in1: Data, in2: Data) -> Data:
                 if rm == RoundingMode.RNE:
-                    in0_rne = cast(RM.RNE, in0)
-                    in1_rne = cast(RM.RNE, in1)
-                    in2_rne = cast(RM.RNE, in2)
+                    in0_rne = cast(RoundingMode_hwtypes.RNE, in0)
+                    in1_rne = cast(RoundingMode_hwtypes.RNE, in1)
+                    in2_rne = cast(RoundingMode_hwtypes.RNE, in2)
                     out_rne = in0_rne.fp_fma(in1_rne, in2_rne)
                     out = out_rne.reinterpret_as_bv()
                 elif rm == RoundingMode.RTZ:
-                    in0_rtz = cast(RM.RTZ, in0)
-                    in1_rtz = cast(RM.RTZ, in1)
-                    in2_rtz = cast(RM.RTZ, in2)
+                    in0_rtz = cast(RoundingMode_hwtypes.RTZ, in0)
+                    in1_rtz = cast(RoundingMode_hwtypes.RTZ, in1)
+                    in2_rtz = cast(RoundingMode_hwtypes.RTZ, in2)
                     out_rtz = in0_rtz.fp_fma(in1_rtz, in2_rtz)
                     out = out_rtz.reinterpret_as_bv()
                 elif rm == RoundingMode.RDN:
-                    in0_rdn = cast(RM.RDN, in0)
-                    in1_rdn = cast(RM.RDN, in1)
-                    in2_rdn = cast(RM.RDN, in2)
+                    in0_rdn = cast(RoundingMode_hwtypes.RDN, in0)
+                    in1_rdn = cast(RoundingMode_hwtypes.RDN, in1)
+                    in2_rdn = cast(RoundingMode_hwtypes.RDN, in2)
                     out_rdn = in0_rdn.fp_fma(in1_rdn, in2_rdn)
                     out = out_rdn.reinterpret_as_bv()
                 elif rm == RoundingMode.RUP:
-                    in0_rup = cast(RM.RUP, in0)
-                    in1_rup = cast(RM.RUP, in1)
-                    in2_rup = cast(RM.RUP, in2)
+                    in0_rup = cast(RoundingMode_hwtypes.RUP, in0)
+                    in1_rup = cast(RoundingMode_hwtypes.RUP, in1)
+                    in2_rup = cast(RoundingMode_hwtypes.RUP, in2)
                     out_rup = in0_rup.fp_fma(in1_rup, in2_rup)
                     out = out_rup.reinterpret_as_bv()
                 else:
                     assert  rm == RoundingMode.RMM
-                    in0_rmm = cast(RM.RUP, in0)
-                    in1_rmm = cast(RM.RUP, in1)
-                    in2_rmm = cast(RM.RUP, in2)
+                    in0_rmm = cast(RoundingMode_hwtypes.RUP, in0)
+                    in1_rmm = cast(RoundingMode_hwtypes.RUP, in1)
+                    in2_rmm = cast(RoundingMode_hwtypes.RUP, in2)
                     out_rmm = in0_rmm.fp_fma(in1_rmm, in2_rmm)
                     out = out_rmm.reinterpret_as_bv()
                 return out
@@ -300,18 +281,73 @@ def float_lib_gen(exp_bits: int, frac_bits: int, ieee_compliance: bool = False):
 
 
     #Used to create floating point ops with a constant rounding mode
+
+
     @lru_cache(None)
     def const_rm(rm: RoundingMode):
         assert isinstance(rm, RoundingMode)
+
+        def gen_const_rm_unary(op_name):
+            @family_closure
+            def fc(family):
+                cast = _get_cast_const_rm(rm, family)
+
+                @family.assemble(locals(), globals())
+                class Op(Peak, BlackBox):
+                    def __call__(self, in_: Data) -> Data:
+                        in_float = cast(in_)
+                        out_float = getattr(in_float, op_name)()
+                        out = out_float.reinterpret_as_bv()
+                        return out
+
+                return Op
+
+            return fc
+
+        def gen_const_rm_binary(op_name):
+            @family_closure
+            def fc(family):
+                cast = _get_cast_const_rm(rm, family)
+
+                @family.assemble(locals(), globals())
+                class Op(Peak, BlackBox):
+                    def __call__(self, in0: Data, in1: Data) -> Data:
+                        in0_float = cast(in0)
+                        in1_float = cast(in1)
+                        out_float = getattr(in0_float, op_name)(in1_float)
+                        out = out_float.reinterpret_as_bv()
+                        return out
+
+                return Op
+
+            return fc
+
+        def gen_const_rm_binary_bit(op_name):
+            @family_closure
+            def fc(family):
+                cast = _get_cast_const_rm(rm, family)
+
+                @family.assemble(locals(), globals())
+                class Op(Peak, BlackBox):
+                    def __call__(self, in0: Data, in1: Data) -> Bit:
+                        in0_float = cast(in0)
+                        in1_float = cast(in1)
+                        out_bit = getattr(in0_float, op_name)(in1_float)
+                        return out_bit
+
+                return Op
+
+            return fc
+
         closures_ = {}
         for k, f in AbstractFPVector.__dict__.items():
             if k.startswith('fp_'):
                 if len(inspect.signature(f).parameters) == 1:
-                    closures_[_format_name(k)] = gen_const_rm_unary(k, rm, closures)
+                    closures_[_format_name(k)] = gen_const_rm_unary(k)
                 elif k in ("fp_leq", "fp_lt", "fp_geq", "fp_gt", "fp_eq"):
-                    closures_[_format_name(k)] = gen_const_rm_binary_bit(k, rm, closures)
+                    closures_[_format_name(k)] = gen_const_rm_binary_bit(k)
                 elif len(inspect.signature(f).parameters) == 2:
-                    closures_[_format_name(k)] = gen_const_rm_binary(k, rm, closures)
+                    closures_[_format_name(k)] = gen_const_rm_binary(k)
 
         @family_closure
         def _fp_fma(family):
