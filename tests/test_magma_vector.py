d5cfd1c4e4daa700a22f0e6e138fe3e02bdb0d25
steveri <steveri@steveri.com>
2023-04-20 11:15:56 -0700

    snapshot

--- tests/test_magma_vector.py
+++ tests/test_magma_vector.py
@@ -27,6 +27,32 @@ from examples.demo_pes.pe6.sim import Inst
 class SimpleMagmaProtocolMeta(MagmaProtocolMeta):
     _CACHE = {}
 
+#     def __new__(cls, name, bases, namespace, info=(None, None), **kwargs):
+#         pass
+#     def __getitem__(cls, direction: m.Direction) -> 'MagmaBitMeta':
+
+    @property
+    def is_directed(cls) ->  'Bool':
+        pass
+
+    @property
+    def undirected_t(cls) -> 'MagmaBitMeta':
+        pass
+
+#     def _to_magma_(cls): 
+#         pass
+
+#     def _qualify_magma_(cls, d): 
+#         pass
+
+
+#     def _flip_magma_(cls): 
+#         pass
+
+#     def _from_magma_value_(cls, value):
+#         pass
+
+
     def _to_magma_(cls):
         return cls.T
 
@@ -61,6 +87,77 @@ class SimpleMagmaProtocol(MagmaProtocol, metaclass=SimpleMagmaProtocolMeta):
             val = self.T()
         self._val = val
 
+
+
+    @staticmethod
+    def get_family():
+        return
+
+#     def __init__(self, *args, **kwargs):
+#         return
+#         pass
+#         self._value=m.Bit(*args, **kwargs)
+
+    def __repr__(self):
+        return
+        pass
+        return self._value.__repr__()
+
+    @property
+    def value(self):
+        return
+        pass
+        return self._value
+
+    # @bit_cast
+    def __eq__(self, other : 'MagmaBit') -> 'MagmaBit':
+        return
+        pass
+        return type(self)(self.value.__eq__(other.value))
+
+    # @bit_cast
+    def __ne__(self, other : 'MagmaBit') -> 'MagmaBit':
+        return
+        return type(self)(self.value.__ne__(other.value))
+
+    def __invert__(self) -> 'MagmaBit':
+        return
+        return type(self)(self.value.__invert__())
+
+    # @bit_cast
+    def __and__(self, other : 'MagmaBit') -> 'MagmaBit':
+        return
+        return type(self)(self.value.__and__(other.value))
+
+    # @bit_cast
+    def __or__(self, other : 'MagmaBit') -> 'MagmaBit':
+        return
+        return type(self)(self.value.__or__(other.value))
+
+    # @bit_cast
+    def __xor__(self, other : 'MagmaBit') -> 'MagmaBit':
+        return
+        return type(self)(self.value.__xor__(other.value))
+
+    def ite(self, t_branch, f_branch):
+        return
+        return type(self)(self.value.ite(t_branch, f_branch))
+
+#         def _ite(select, t_branch, f_branch):
+#             return smt.Ite(select.value, t_branch.value, f_branch.value)
+#         return build_ite(_ite, self, t_branch, f_branch)
+
+    def __bool__(self):
+        return
+        raise TypeError('MagmaBit cannot be converted to bool')
+
+        
+
+
+
+
+
+
     def _get_magma_value_(self):
         return self._val
 
@@ -72,13 +169,13 @@ class SimpleMagmaProtocol(MagmaProtocol, metaclass=SimpleMagmaProtocolMeta):
 
 def test_foo():
     class bitfoo(m.Circuit):
-#         io = m.IO(
-#             I=m.In( SimpleMagmaProtocol[m.Bit]), 
-#             O=m.Out(SimpleMagmaProtocol[m.Bit])
-#         )
-         io = m.IO(
-             I=m.In( MagmaBit ), 
-             O=m.Out(MagmaBit )
-         )
+        io = m.IO(
+            I=m.In( SimpleMagmaProtocol[m.Bit]), 
+            O=m.Out(SimpleMagmaProtocol[m.Bit])
+        )
+#          io = m.IO(
+#              I=m.In( MagmaBit ), 
+#              O=m.Out(MagmaBit )
+#          )
         
         
